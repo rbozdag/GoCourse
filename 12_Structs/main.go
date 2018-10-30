077e3acff542de6a973a685c6873f7d2304563aa
RAHMIBO <rahmi.bozdag@intertech.com.tr>
2018-10-30 22:39:34 +0300

    structs course

---
+++ 12_Structs/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	city      string
+	age       int
+}
+
+func main() {
+	//descriptive way
+	user := User{firstName: "Rahmi", lastName: "Bozdağ", city: "İstanbul", age: 30}
+	fmt.Println(user, " --> descriptive way")
+
+	fmt.Println("----------------------------------------")
+
+	//short way
+	userShort := User{"Rahmi", "Bozdağ", "İstanbul", 30}
+	fmt.Println(userShort, " --> short way")
+
+	fmt.Println("----------------------------------------")
+
+	//assign with copy
+	userCopy := user
+	userCopy.age++
+	fmt.Println(user, " --> original variable")
+	fmt.Println(userCopy, " -> copy of variable")
+
+	fmt.Println("----------------------------------------")
+
+	//pointer
+	userPointer := &user
+	(*userPointer).age *= 2
+	fmt.Println(user, " --> original variable")
+	fmt.Println(*userPointer, " -> pointer")
+
+}
